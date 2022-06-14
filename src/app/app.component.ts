d087a84dd03c01d1eea4939cf6ca3a4c6c7ce48f
190252 <190252@studierende.htl-donaustadt.at>
2022-06-14 15:30:55 +0200

    Ausgabe der Karten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card, CARDS} from "./models/card";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Panzer';
+
+  cards=CARDS;
+
+
 }
--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,2 +1,25 @@
 export class Card {
+
+  cards: number;
+  titleT: string;
+  quantityT: Number;
+  heightT: Number;
+  lenghtT: Number;
+  widthT: Number;
+
+
+  constructor(cards: number, titleT: string, quantityT: Number, heightT: Number, lenghtT: Number, widthT: Number) {
+    this.cards = cards;
+    this.titleT = titleT;
+    this.quantityT = quantityT;
+    this.heightT = heightT;
+    this.lenghtT = lenghtT;
+    this.widthT = widthT;
+  }
 }
+
+export const CARDS: Card[]=[
+  new Card(1,"Tank",5,80,40,6000),
+  new Card(2,"Tank2",10,70,30,5000),
+  new Card(3,"Tank3",15,50,60,8000),
+]
